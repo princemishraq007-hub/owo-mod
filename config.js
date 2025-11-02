9177cdb4045ab78fc09dd71c903d4d14353a206e
princemishraq007-hub <princemishraq007@gmail.com>
2025-11-02 10:34:30 +0530

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "",
-    channel: "",
+    token: "MTIzMTI0NDIxMzQ0MjgzODU5MQ.GWTTX1.vTkcStJaz1jyZmddvO3V9ykBiVEYU94pv83fPM",
+    channel: "1245673803347001416",
     data: "data.json",
     cf: {
         enable: true,
